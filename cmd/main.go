f23ecc6669b7c9f6edabf3b25c8887df3e7b3922
Vitaly Petrov <v31337@gmail.com>
2022-07-19 18:40:10 +0300

    envvars args

--- cmd/main.go
+++ cmd/main.go
@@ -108,7 +108,7 @@ func main() {
 
 	dt := time.Now()
 	log.Println("")
-	log.Printf("Started: %s, Version: %s, Stun: %s, In: %s, Out: %s\n", dt.Format(time.RFC3339), version, *stunServer, *inFileName, *outFileName)
+	log.Printf("Started: %s, Version: %s, Stun: %s, In: %s, Out: %s\n", dt.Format(time.RFC3339), version, stunServer, inFileName, outFileName)
 	log.Println("Init video provider and encoders")
 
 	// setup major services
